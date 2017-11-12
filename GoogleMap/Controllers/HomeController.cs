2bb11b77a9a668b09a15fefadfe685550465919c
JULIO-PC\JULIO <julio.limachi3@gmail.com>
2017-11-12 01:28:12 -0500

    version 1.0
    subir proyecto

---
+++ GoogleMap/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using GoogleMap.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoogleMap.Controllers
+{
+    public class HomeController : Controller
+    {
+        TestDbEntities context = new TestDbEntities();
+        public ActionResult Index()
+        {
+            ViewBag.ListOfDropdown = context.google_map.ToList();
+            return View();
+        }
+        public JsonResult GetAllLocation()
+        {
+            var data = context.google_map.ToList();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult GetAllLocationById(int id)
+        {
+            var data = context.google_map.Where(x => x.Id == id).SingleOrDefault();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
